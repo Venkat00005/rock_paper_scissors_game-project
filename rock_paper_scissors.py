cc0fad2fb459e0c4b9f1b9eaf644c75a8565e5a3
Venkat00005 <109608383+Venkat00005@users.noreply.github.com>
2022-12-09 13:31:13 +0530

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,69 +1,140 @@
 import random
-computer = 0
-user = 0
+import time
+computer_score = 0
+user_score = 0
 user_name = input("enter player name:")
 computer_choice = ["rock","paper","scissors"] 
-while computer<3 and user<3:            #CAN MODIFY THE TOTAL POINTS NEEDED TO WIN IN THE GAME HERE
+print('''Lets Play!!!
+| | |
+| | |
+| | |
+V V V''')
+while computer_score<3 and user_score<3:            #CAN MODIFY THE TOTAL POINTS NEEDED TO WIN IN THE GAME HERE
 
-  user_choice=input("choose your choice rock,paper,scissors:\n") #USER INPUT
+  user_choice=input("select your choice rock,paper,scissors:\n") #USER INPUT
   user_choice = user_choice.lower()
   random.shuffle(computer_choice)                                     #COMPUTER'S RANDOM INPUT
 
   if computer_choice[1]=="rock" and user_choice=='paper':
-    user=user+1
+    user_score=user_score+1
+    time.sleep(1)
     print("computer=",computer_choice[1])                   #USER WILL GET POINT HERE
-    print("computer=", computer)
-    print(user_name,"=",user)
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)
+    print(user_name,"score=",user_score)
+    print('''
+| | |
+| | |
+| | |
+V V V''')
 
   elif computer_choice[1]=="rock" and user_choice=='scissors':
-    computer=computer+1
+    computer_score=computer_score+1
+    time.sleep(1)
     print("computer=",computer_choice[1])                   #COMPUTER SCORED POINT HERE
-    print("computer=", computer)
-    print(user_name,"=",user)
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)
+    print(user_name,"score=",user_score)
+    print('''
+| | |
+| | |
+| | |
+V V V''')
 
   elif computer_choice[1]=="rock" and user_choice=='rock':
+    time.sleep(1)
     print("computer=",computer_choice[1])
-    print("computer=", computer)             
-    print(user_name,"=",user)
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)             
+    print(user_name,"score=",user_score)
+    print('''
+| | |
+| | |
+| | |
+V V V''')
     continue                                  #COMPUTER AND USER CHOSE THE SAME OPTION
 
   elif computer_choice[1]=="paper" and user_choice=='rock':
-    computer=computer+1
+    computer_score=computer_score+1
+    time.sleep(1)
     print("computer=",computer_choice[1])                   #COMPUTER SCORED
-    print("computer=", computer)
-    print(user_name,"=",user)
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)
+    print(user_name,"score=",user_score)
+    print('''
+| | |
+| | |
+| | |
+V V V''')
 
   elif computer_choice[1]=="paper" and user_choice=='paper':
+    time.sleep(1)
     print("computer=",computer_choice[1])
-    print("computer=", computer)
-    print(user_name,"=",user)  
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)
+    print(user_name,"score=",user_score)  
+    print('''
+| | |
+| | |
+| | |
+V V V''')
     continue                                  #TIE
 
   elif computer_choice[1]=="paper" and user_choice=='scissors':
-    user=user+1                                 #USER SCORED
+    user_score=user_score+1                                 #USER SCORED
+    time.sleep(1)
     print("computer=",computer_choice[1])
-    print("computer=", computer)
-    print(user_name,"=",user)
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)
+    print(user_name,"score=",user_score)
+    print('''
+| | |
+| | |
+| | |
+V V V''')
 
   elif computer_choice[1]=="scissors" and user_choice=='rock':
-    user=user+1                                #USER SCORED
+    user_score=user_score+1                                #USER SCORED
+    time.sleep(1)
     print("computer=",computer_choice[1])
-    print("computer=", computer)
-    print(user_name,"=",user)
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)
+    print(user_name,"score=",user_score)
+    print('''
+| | |
+| | |
+| | |
+V V V''')
 
   elif computer_choice[1]=="scissors" and user_choice=='paper':
-    computer=computer+1                      #COMPUTER SCORED
+    computer_score=computer_score+1                      #COMPUTER SCORED
+    time.sleep(1)
     print("computer=",computer_choice[1])
-    print("computer=", computer)
-    print(user_name,"=",user)
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)
+    print(user_name,"score=",user_score)
+    print('''
+| | |
+| | |
+| | |
+V V V''')
 
   elif computer_choice[1]=="scissors" and user_choice=='scissors': 
+    time.sleep(1)
     print("computer=",computer_choice[1])
-    print("computer=", computer)
-    print(user_name,"=",user)
+    time.sleep(2)
+    print("\ncomputer score=", computer_score)
+    print(user_name,"score=",user_score)
+    print('''
+| | |
+| | |
+| | |
+V V V''')
     continue                                 #TIE
-    
-if computer==3:                               #COMPUTER WINNING CONDITION AND CAN ALSO MODIFY ACCORDINGLY
+
+time.sleep(2)
+if computer_score==3:                               #COMPUTER WINNING CONDITION AND CAN ALSO MODIFY ACCORDINGLY
   print("computer is winner") 
 else:
   print(user_name,"is winner")                     #USER WINNER
+print("Game ended, Thanks for playing")
